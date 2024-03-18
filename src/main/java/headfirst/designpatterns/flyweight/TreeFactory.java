bc0a19080f99e9bca136694d2c76e1c8976769ab
zhengshi.wang <zhengshi.wang@trip.com>
2024-03-18 16:52:34 +0800

    享元模式

--- src/main/java/headfirst/designpatterns/flyweight/TreeFactory.java
+++ src/main/java/headfirst/designpatterns/flyweight/TreeFactory.java
@@ -1,18 +1,25 @@
 package headfirst.designpatterns.flyweight;
 
+import cn.hutool.core.util.ObjectUtil;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class TreeFactory {
-	Tree d, c = null;
-	public TreeFactory() {
-		this.d = new DeciduousTree();
-		this.c = new ConiferTree();
-	}
-	public Tree getTree(String type) throws Exception {
-		if (type.equals("deciduous")) {
-			return this.d;
-		} else if (type.equals("conifer")) {
-			return this.c;
-		} else {
-			throw new Exception("Invalid kind of tree");
-		}
-	}
+    Map<String, Tree> trees = new HashMap<>();
+
+    public Tree getTree(String type) throws Exception {
+        Tree tree = trees.get(type);
+        if (ObjectUtil.isNotEmpty(tree)) {
+            return tree;
+        }
+        if (type.equals("deciduous")) {
+            trees.put(type, new DeciduousTree());
+        } else if (type.equals("conifer")) {
+            trees.put(type, new ConiferTree());
+        } else {
+            throw new Exception("Invalid kind of tree");
+        }
+        return trees.get(type);
+    }
 }
